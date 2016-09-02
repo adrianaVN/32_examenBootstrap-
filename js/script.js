8ff4b1a7268a05bf31a72de5fc9d4d550c2d796e
adrianaVN <venegasadriana78@gmail.com>
2016-09-02 19:31:29 +0000

    inicio examen

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});*/
+(function(document , window , $){
+	$(document).ready(function(){
+		$(".slider").slick();
+	});
+})(document , window , jQuery);
